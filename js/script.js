6e747b24a7d422c40576587dce024ff1ded64607
bchizi <leonbichanga@gmail.com>
2019-09-14 23:58:51 +0300

    added the js code

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function add(no1, no2) {
+    alert(no1 + no2);
+}
+var a = parseInt(prompt("add"))
+var b = parseInt(prompt("add"))
+add(a, b);
+
+function subtract(no1, no2) {
+    alert(no1 - no2);
+}
+var a = parseInt(prompt("minus"))
+var b = parseInt(prompt("minus"))
+subtract(a, b);
+
+function mult(no1, no2) {
+    alert(no * no2);
+}
+var a = parseInt(prompt("multiply"))
+var b = parseInt(prompt("multiply"))
+divide(a, b);
+
+function divide(no1, no2) {
+    alert(no1 / no2);
+}
+var a = parseInt(prompt("divide"))
+var b = parseInt(prompt("divide"))
+add(a, b);
